9d5fee05ad6222909722709f7213a4a8d9ee4c60
Eleanor Brodie <eabrodie@hotmail.co.uk>
2015-10-11 17:23:02 +0100

    Second lesson

--- bundle.js
+++ bundle.js
@@ -1,10 +1,23 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+var gcd = require('gcd');
+
+function gcdprocess() {
+  var input1 = parseFloat(document.getElementById('input1').value);
+  var input2 = parseFloat(document.getElementById('input2').value);
+  var answer = gcd(input1, input2);
+  var result = document.getElementById('result');
+  result.firstChild.nodeValue = answer;
+  //console.log(answer);
+};
+
+
 function addition() {
   var input1 = parseFloat(document.getElementById('input1').value);
   var input2 = parseFloat(document.getElementById('input2').value);
   var answer = input1 + input2;
   var result = document.getElementById('result');
   result.firstChild.nodeValue = answer;
-  console.log(answer);
+  //console.log(answer);
 };
 
 
@@ -14,7 +27,7 @@ function subtraction() {
   var answer = input1 - input2;
   var result = document.getElementById('result');
   result.firstChild.nodeValue = answer;
-  console.log(answer);
+  //console.log(answer);
 };
 
 function multiplication() {
@@ -23,7 +36,7 @@ function multiplication() {
   var answer = input1 * input2;
   var result = document.getElementById('result');
   result.firstChild.nodeValue = answer;
-  console.log(answer);
+  //console.log(answer);
 };
 
 function division() {
@@ -32,7 +45,7 @@ function division() {
   var answer = input1 / input2;
   var result = document.getElementById('result');
   result.firstChild.nodeValue = answer;
-  console.log(answer);
+  //console.log(answer);
 };
 
 
@@ -49,4 +62,14 @@ window.onload = function() {
   
   var divide = document.getElementById("divide");
   divide.addEventListener("click", division, false);
+  
+  var gcdbutton = document.getElementById("gcd-button");
+  gcdbutton.addEventListener("click", gcdprocess, false);
 }
+},{"gcd":2}],2:[function(require,module,exports){
+module.exports = function gcd (a, b) {
+    if (b === 0) return a;
+    return gcd(b, a % b);
+};
+
+},{}]},{},[1]);
--- index2.js
+++ index2.js
@@ -1,10 +1,22 @@
+var gcd = require('gcd');
+
+function gcdprocess() {
+  var input1 = parseFloat(document.getElementById('input1').value);
+  var input2 = parseFloat(document.getElementById('input2').value);
+  var answer = gcd(input1, input2);
+  var result = document.getElementById('result');
+  result.firstChild.nodeValue = answer;
+  //console.log(answer);
+};
+
+
 function addition() {
   var input1 = parseFloat(document.getElementById('input1').value);
   var input2 = parseFloat(document.getElementById('input2').value);
   var answer = input1 + input2;
   var result = document.getElementById('result');
   result.firstChild.nodeValue = answer;
-  console.log(answer);
+  //console.log(answer);
 };
 
 
@@ -14,7 +26,7 @@ function subtraction() {
   var answer = input1 - input2;
   var result = document.getElementById('result');
   result.firstChild.nodeValue = answer;
-  console.log(answer);
+  //console.log(answer);
 };
 
 function multiplication() {
@@ -23,7 +35,7 @@ function multiplication() {
   var answer = input1 * input2;
   var result = document.getElementById('result');
   result.firstChild.nodeValue = answer;
-  console.log(answer);
+  //console.log(answer);
 };
 
 function division() {
@@ -32,7 +44,7 @@ function division() {
   var answer = input1 / input2;
   var result = document.getElementById('result');
   result.firstChild.nodeValue = answer;
-  console.log(answer);
+  //console.log(answer);
 };
 
 
@@ -49,4 +61,7 @@ window.onload = function() {
   
   var divide = document.getElementById("divide");
   divide.addEventListener("click", division, false);
+  
+  var gcdbutton = document.getElementById("gcd-button");
+  gcdbutton.addEventListener("click", gcdprocess, false);
 }
